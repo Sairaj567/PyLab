f9ecfb545cc978a8ab71b1a95d0bd2a7ff74f736
Sairaj567 <95161093+Sairaj567@users.noreply.github.com>
2024-01-15 11:12:10 +0530

    Add files via upload

---
+++ EXP1/list.py
@@ -0,0 +1,35 @@
+#making a list
+pets = ["omm" , "sahil ","ayush" ,"sashank", "sarvesh"]
+
+
+#sorting
+pets.sort()
+print(pets)
+
+#print index
+print(pets[1])
+
+#extend
+a = "aayush"
+pets.extend(a)
+print(pets)
+
+#remove
+pets.remove("omm")
+print(pets)
+
+#insert
+pets.insert(1, "aayush")
+print(pets)
+
+#append
+pets.append("aryan")
+print(pets)
+
+#pop end
+pets.pop()
+print(pets)
+
+#sorting
+pets.sort()
+print(pets)
